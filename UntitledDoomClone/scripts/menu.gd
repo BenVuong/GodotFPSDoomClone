b834810574f4ae897e14734a63c4c6b6b701516a
Ben G Vuong <ben.vuong2000@gmail.com>
2023-07-07 12:29:17 -0500

    Added basic Main menu

---
+++ UntitledDoomClone/scripts/menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
